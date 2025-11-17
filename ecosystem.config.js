877dfd33d21ac4e225da0f26fd15f7832b0877a4
Miftah Steven <miftahsteven@gmail.com>
2025-11-17 12:56:49 +0700

    commit ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: 'api-mrb-supplydata',
-      script: 'build/index.js', // jalankan hasil build
-      watch: false,             // matikan watch di production
-      instances: 1,             // bisa ubah ke 'max' untuk cluster mode
+      name: 'api-mrb',
+      script: 'src/index.ts',
+      interpreter: '/usr/local/bin/ts-node',
+      interpreter_args: '-r tsconfig-paths/register',
+      watch: true,
       env: {
         NODE_ENV: 'development',
-        PORT: 8044,
+        PORT: 8044
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 8044,
+        PORT: 8044
       },
-      error_file: './logs/error.log',
-      out_file: './logs/out.log',
-      time: true,
     },
   ],
 };
